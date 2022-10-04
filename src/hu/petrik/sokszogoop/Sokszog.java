afb6b1c76e2c65a2a08c35cc177c83ca79183e97
Varga Zsigmond <petrikdiak@gmail.com>
2022-10-04 14:20:28 +0200

    getter setter konstruktor

--- src/hu/petrik/sokszogoop/Sokszog.java
+++ src/hu/petrik/sokszogoop/Sokszog.java
@@ -2,4 +2,16 @@ package hu.petrik.sokszogoop;
 
 public class Sokszog {
     private double a;
+
+    public Sokszog(double a) {
+        this.a = a;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
 }
